4e3adb1936b31cf1afa160051018c9426374c761
pjf <pjf@461bd5db-b6cf-484b-b731-06a28823be7a>
2008-02-10 22:48:34 +0000

    Updated POD

--- lib/Data/Validation.pm
+++ lib/Data/Validation.pm
@@ -4,11 +4,11 @@ package Data::Validation;
 
 use strict;
 use warnings;
-use charnames qw(:full);
-use base qw(Class::Accessor::Fast);
-use English qw(-no_match_vars);
+use charnames      qw(:full);
+use base           qw(Class::Accessor::Fast);
+use English        qw(-no_match_vars);
 use Regexp::Common qw(number);
-use Scalar::Util qw(looks_like_number);
+use Scalar::Util   qw(looks_like_number);
 
 use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev$ =~ /\d+/gmx );
 
@@ -222,6 +222,69 @@ sub _will {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 Name
+
+Data::Validation - Check data values form conformance with constraints
+
+=head1 Version
+
+0.1.$Revision$
+
+=head1 Synopsis
+
+   use Data::Validation;
+
+=head1 Description
+
+=head1 Subroutines/Methods
+
+=head1 Diagnostics
+
+=head1 Configuration and Environment
+
+=head1 Dependencies
+
+=over 4
+
+=item L<Class::Accessor::Fast>
+
+=item L<Regexp::Common>
+
+=item L<Scalar::Util>
+
+=back
+
+=head1 Incompatibilities
+
+There are no known incompatibilities in this module.
+
+=head1 Bugs and Limitations
+
+There are no known bugs in this module.
+Please report problems to the address below.
+Patches are welcome.
+
+=head1 Author
+
+Peter Flanigan, C<< <Support at RoxSoft.co.uk> >>
+
+=head1 License and Copyright
+
+Copyright (c) 2007 RoxSoft Limited. All rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself. See L<perlartistic>.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+=cut
 
 # Local Variables:
 # mode: perl
