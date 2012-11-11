1a7b63023827a4d718ccdb9fd26fa7ba514c206f
pjf <pjf@461bd5db-b6cf-484b-b731-06a28823be7a>
2012-11-11 19:02:23 +0000

    Added grahamc linux grosics to test exception. Broken resolver 49ebef50-1ad2-11e2-ab43-8db1a6229e16 Updated inc::Bob to 1.6

--- inc/CPANTesting.pm
+++ inc/CPANTesting.pm
@@ -1,4 +1,5 @@
 # @(#)$Id$
+# Bob-Version: 1.6
 
 package CPANTesting;
 
@@ -7,26 +8,26 @@ use warnings;
 
 use Sys::Hostname; my $host = lc hostname; my $osname = lc $^O;
 
+# Is this an attempted install on a CPAN testing platform?
+sub is_testing { !! ($ENV{AUTOMATED_TESTING} || $ENV{PERL_CR_SMOKER_CURRENT}
+                 || ($ENV{PERL5OPT} || q()) =~ m{ CPAN-Reporter }mx) }
+
 sub should_abort {
    return 0;
 }
 
 sub test_exceptions {
-   my $p = shift; __is_testing() or return 0;
+   my $p = shift; is_testing() or return 0;
 
    $p->{stop_tests} and return 'CPAN Testing stopped in Build.PL';
 
-   $osname eq q(mirbsd) and return 'Mirbsd OS unsupported';
+   $osname eq q(mirbsd) and return 'Mirbsd  OS unsupported';
    $osname eq q(linux)  and $host eq q(k83)
-                        and return 'Stopped andk linux k83 - broken resolver';
+      and return "Stopped andk ${osname} ${host} - broken resolver";
+   $osname eq q(linux)  and $host eq q(grosics)
+      and return "Stopped grahmac ${osname} ${host} - broken resolver";
    return 0;
 }
-
-# Private functions
-
-# Is this an attempted install on a CPAN testing platform?
-sub __is_testing { !! ($ENV{AUTOMATED_TESTING} || $ENV{PERL_CR_SMOKER_CURRENT}
-                   || ($ENV{PERL5OPT} || q()) =~ m{ CPAN-Reporter }mx) }
 
 1;
 
