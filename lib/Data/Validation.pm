cfa469630d5d3f44d06e80ed2c4ae2bd149d8700
pjfl <github@roxsoft.co.uk>
2014-10-03 01:23:42 +0100

    0.16 Added git hooks

--- lib/Data/Validation.pm
+++ lib/Data/Validation.pm
@@ -2,7 +2,7 @@ package Data::Validation;
 
 use 5.010001;
 use namespace::autoclean;
-use version; our $VERSION = qv( sprintf '0.16.%d', q$Rev: 1 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.16.%d', q$Rev: 2 $ =~ /\d+/gmx );
 
 use Moo;
 use Data::Validation::Constants;
@@ -158,7 +158,7 @@ Data::Validation - Filter and validate data values
 
 =head1 Version
 
-Describes version v0.16.$Rev: 1 $ of L<Data::Validation>
+Describes version v0.16.$Rev: 2 $ of L<Data::Validation>
 
 =head1 Synopsis
 
